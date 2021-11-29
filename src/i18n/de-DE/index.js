7f880a61cc92ee69dbf92eaf38b10e309ecafb60
jo <jo>
2021-11-29 23:26:55 +0100

    i18n translations languages added

---
+++ src/i18n/de-DE/index.js
@@ -0,0 +1,220 @@
+// This is just an example,
+// so you can safely delete all default props below
+
+export default {
+  "ok": "Ok",
+  "cancel": "Abbruch",
+  "error_alert_title": "Huuups...",
+  "error_alert_text": "Irgendwas lief falsch, versuche es erneut",
+  "token_expired_alert_title": "Session Abgelaufen!",
+  "token_expired_alert_text": "Bitte einloggen um weiter zu machen",
+  "entry": "Anmelden",
+  "login": "Anmelden",
+  "page_not_found": "Seite nicht gefunden",
+  "go_home": "Zur Startseite",
+  "email": "E-Mail",
+  "your_email": "Ihre E-Mail Adresse",
+  "check_mails": "E-Mail wurde versendet",
+  "email sent": "E-Mail versendet",
+  "remember_me": "Angemeldet bleiben",
+  "password": "Passwort",
+  "forgot_password": "Passwort vergessen",
+  "forgot_password_text": "Haben Sie ihr",
+  "forgot_password_link": "Passwort vergessen?",
+  "confirm_password": "Passwort wiederholen",
+  "name": "Name",
+  "save": "speichern",
+  "edit": "Bearbeiten",
+  "empty": "leer",
+  "delete": "löschen",
+  "your_name": "Ihr Name",
+  "toggle_navigation": "Navigation umschalten",
+  "you_are_logged_in": "Sie sind angemeldet!",
+  "reset_password": "Passwort zurücksetzen",
+  "send_password_reset_link": "Sende Passwort-Zurücksetzen-Link",
+  "prm_title": "Passwort Zurücksetzen",
+  "prm_desc": "Falls Sie Ihr Passwort vergessen haben, geben Sei unten Ihre E-Mail Adresse des Kontos ein und folgen Sie die Schritte innerhalb E-Mail",
+  "pr_title": "Neues Passwort",
+  "pr_desc": "Vergeben Sie ein neues Passwort für Ihr Benutzerkonto",
+  "settings": "Einstellungen",
+  "your settings": "Ihre Einstellungen",
+  "profile": {
+      "title": "Profil",
+      "h1": "Profil",
+      "company": {
+          "create": "Erstelle Firma",
+          "choose": "Firma auswählen"
+      },
+      "department": {
+          "create": "Erstelle eine Abteilung",
+          "choose": "Wähle eine Abteilung"
+      },
+      "location": {
+          "create": "Erstelle einen Ort",
+          "choose": "Wähle einen Ort"
+      }
+  },
+  "validation": {
+      "length": {
+          "short": "Diese Eingabe ist zu kurz"
+      }
+  },
+  "your_info": "Ihre Infos",
+  "info_updated": "Ihre Infos wurden aktualisiert!",
+  "update": "Speichern",
+  "your_password": "Ihr Passwort",
+  "password_updated": "Ihr Passwort wurde geändert!",
+  "new_password": "Neues Passwort",
+  "login_with": "Anmelden mit",
+  "verify_email": "E-Mail verifizieren",
+  "verify_email_desc": "Versenden Sie einen Link an Ihre untenstehende E-Mail-Adresse um Ihr Konto zu verifizieren",
+  "send_verification_link": "Sende Verifikations-Link",
+  "resend_verification_link": "Verifikations-Link erneut senden?",
+  "failed_to_verify_email": "E-Mail verifikation fehlgeschlagen.",
+  "verify_email_address": "Wir senden dir eine E-Mail mit dem Verifikations-Link.",
+  "You must :linkOpen verify :linkClose your email first.": "aggna",
+  "You must {a} verify {b} your email first.": "usni",
+  "back": "zurück",
+  "authentificate_title": "Authentifizieren",
+  "Unauthenticated": "Abgemeldet",
+  "auth.failed": "Anmeldung fehlgeschlagen",
+  "auth": {
+      "failed": "Anmeldung fehlgeschlagen"
+  },
+  "closer_button": "schließen",
+  "DE": "Deutsch",
+  "EN": "Englisch",
+  "ES": "Spanisch",
+  "中文": "Chinesisch",
+  "pan_and_pin": "PAN & PIN",
+  "pan_title": "PIN & PAN",
+  "pan_desc": "Ich besitze PAN und PIN und möchte mich damit anmelden",
+  "login_title": "Anmelden",
+  "login_desc": "Ich besitze ein Konto und möchte mich darin anmelden",
+  "register": "Registrieren",
+  "register_title": "Registrieren",
+  "register_desc": "Ich will ein neues Konto mit meiner E-Mail Adresse erstellen",
+  "register_with": "Registrieren mit",
+  "register_alternative": "Neu hier?",
+  "free_register": "Jetzt kostenlos registrieren",
+  "with_email": "Mit E-Mail",
+  "PAN": "PAN",
+  "PAN & PIN Combination failed": "PAN & PIN Kombination fehlgeschlagen",
+  "PIN": "PIN",
+  "Welcome": "Willkommen",
+  "id": "ID",
+  "public": "Öffentlich",
+  "created_by": "Erstellt von",
+  "password_label": "Passwort",
+  "email_label": "E-Mail",
+  "Search": "Suchen...",
+  "home": "Startseite",
+  "surveys": "Umfragen",
+  "bottombar": {
+      "menu": "Menü",
+      "profile": "Konto",
+      "settings": "Einstellungen",
+      "home": "Home",
+      "surveys": "Umfragen",
+      "faq": "FAQ"
+  },
+
+  "sidenav": {
+      "subtitle": "Befragungen Online",
+      "home": "Home",
+      "profile": "Konto",
+      "surveys": "Ihre Umfragen",
+      "faq": "FAQ  /  Hilfe",
+      "settings": "Einstellungen",
+
+      "backend": {
+          "title": "Administration",
+          "users": "Benutzer",
+          "pan": "Zugänge",
+          "groups": "Gruppen",
+          "surveys": "Umfragen",
+          "statistics": "Statistiken",
+          "statistic": "Statistiken"
+      }
+  },
+
+  "logout": {
+      "title": "Abmelden",
+      "desc": "Möchten Sie sich abmelden?",
+      "btn": "Abmelden",
+      "btn_stay_here": "Hier bleiben"
+  },
+  "password_hint": "Zeichen",
+  "password_repeat_hint": "Passwort wiederholen",
+  "invalid_given_data": "Die eingegeben Daten waren nicht korrekt",
+  "company": "Firma",
+  "department": "Abteilung",
+  "location": "Ort",
+
+  "newsletter": {
+      "newsletter": "Newsletter"
+  },
+
+  "groups": "Gruppen",
+  "Groups": "Gruppen",
+  "updated_at": "Aktualisiert am",
+  "created_at": "Erstellt am",
+
+  "remove": "Entfernen",
+  "added": "Hinzugefügt",
+  "add_to": "Hinzufügen",
+  "canceled": "Abgebrochen",
+  "attribute_changed": "Attribut angepasst",
+  "attribute_opened": "Attribut Geöffnet",
+  "attribute_unsaved": "Attribut nicht gespeichert",
+  "data_saved": "Daten gespeichert",
+  "data_unsaved": "Daten nicht gespeichert",
+  "reset": "Zurücksetzen",
+  "loading": {
+      "text": "Bitte warten ...",
+      "statistics": "Bitte warten. Die Statistiken werden geladen. Das könnte einen Moment dauern..."
+  },
+  "required": "Dieses Feld ist erforderlich",
+  "years": "Jahre",
+  "months": "Monate",
+  "days": "Tage",
+  "hours": "Stunden",
+  "minutes": "Minuten",
+  "seconds": "Sekunden",
+  "The given data was invalid.": "Die eingegebenen Daten sind nicht korrekt.",
+  "The given data was invalid": "Die eingegebenen Daten sind nicht korrekt",
+  "title": "Titel",
+  "subtitle": "Untertitel",
+  "description": "Beschreibung",
+  "Back to surveys": "Zurück zu den Umfragen",
+  "your_surveys": "Ihre Umfragen",
+  "max_options": "Maximale Optionen",
+  "min_options": "Minimale Optionen",
+  "de": "Deutsch",
+  "en": "Englisch",
+  "es": "Spanisch",
+  "zh-CN": "Chinesisch",
+  "description_mods": "Beschreibung Moderatoren",
+  "description_public": "Beschreibung Öffentlich",
+  "Question": "Frage",
+  "From": "Von",
+  "from": "von",
+  "time since": "Vor",
+  "import comment": "Import-Kommentar",
+  "contact mail": "Kontakt-Mail",
+  "last mail date": "Datum letzter Mail",
+  "last mail status": "Status letzter Mail",
+
+  "welcome_heading": "Willkommen",
+  "welcome_text": "Um sich in ihr Konto anzumelden, klicken Sie auf eine der drei unterstehenden Optionen",
+
+  "faq": {
+      "title": "FAQ / Hilfe",
+      "subtitle": "Fragen & Antworten",
+      "text": "Bei Fragen oder Hilfestellung, melden Sie sich via E-Mail bei <a href='mailto:{email}'>{email}</a>"
+  },
+
+  "management": {
+      "header": "Verwaltung"
+  }
+}
--- src/i18n/en-US/index.js
+++ src/i18n/en-US/index.js
@@ -2,6 +2,150 @@
 // so you can safely delete all default props below
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful'
+  "failed": 'Action failed',
+  "success": 'Action was successful',
+
+  "ok": "Ok",
+  "cancel": "Cancel",
+  "error_alert_title": "Oops...",
+  "error_alert_text": "Something went wrong! Please try again.",
+  "token_expired_alert_title": "Session Expired!",
+  "token_expired_alert_text": "Please log in again to continue.",
+  "entry": "Log In",
+  "login": "Log In",
+  "page_not_found": "Page Not Found",
+  "go_home": "Go Home",
+  "email": "Email",
+  "remember_me": "Remember Me",
+  "password": "Password",
+  "forgot_password": "Forgot Your Password?",
+  "forgot_password_text": "Did you forget",
+  "forgot_password_link": "your password?",
+  "confirm_password": "Confirm Password",
+  "with_email": "With Email",
+  "name": "Name",
+  "toggle_navigation": "Toggle navigation",
+  "home": "Home",
+  "surveys": "Surveys",
+  "you_are_logged_in": "You are logged in!",
+  "reset_password": "Reset Password",
+  "send_password_reset_link": "Send Password Reset Link",
+  "settings": "Settings",
+  "profile": {
+      "title": "Profile",
+      "h1": "Profile",
+      "company": {
+          "create": "Create company",
+          "choose": "Choose company"
+      },
+      "department": {
+          "create": "Create department",
+          "choose": "Choose department"
+      },
+      "location": {
+          "create": "Create location",
+          "choose": "Choose location"
+      }
+  },
+  "validation": {
+      "length": {
+          "short": "The input is too short"
+      }
+  },
+  "your_info": "Your Info",
+  "info_updated": "Your info has been updated!",
+  "update": "Update",
+  "your_password": "Your Password",
+  "password_updated": "Your password has been updated!",
+  "new_password": "New Password",
+  "login_with": "Login with",
+  "login_title": "Login",
+  "login_desc": "I have an account and would like to log into it",
+  "password_label": "Password",
+  "email_label": "Email",
+  "register": "Register",
+  "register_with": "Register with",
+  "register_title": "Register",
+  "register_desc": "I want to create a new account with my email address",
+  "register_alternative": "New here?",
+  "free_register": "Sign up now for free",
+
+  "verify_email": "Verify Email",
+  "send_verification_link": "Send Verification Link",
+  "resend_verification_link": "Resend Verification Link ?",
+  "failed_to_verify_email": "Failed to verify email.",
+  "verify_email_address": "We sent you an email with an the verification link.",
+  "authentificate_title": "Authentificate",
+  "Unauthenticated": "Logged out",
+  "auth.failed": "Authentification error",
+  "auth": {
+      "failed": "Authentification error"
+  },
+  "closer_button": "close",
+  "DE": "German",
+  "de": "German",
+  "EN": "English",
+  "en": "English",
+  "ES": "Spanish",
+  "es": "Spanish",
+  "中文": "Chinese",
+  "zh-CN": "Chinese",
+  "pan_title": "PIN & PAN",
+  "pan_desc": "I have a PAN and PIN and would like to use them to log in",
+  "pan_and_pin": "PIN & PAN",
+  "pan_and_pin_desc": "I know my PIN and PAN and want to authentificate with it",
+  "PAN & PIN Combination failed": "PAN & PIN Combination failed",
+  "loading": {
+      "text": "Loading... Please wait",
+      "statistics": "Please wait a moment. Statistics loading. This could take a while..."
+  },
+  "your_surveys": "Your surveys",
+
+  "welcome_heading": "Welcome",
+  "welcome_text": "To log into your account, click on one of the three options below",
+
+  "bottombar": {
+      "menu": "Menu",
+      "profile": "Profile",
+      "settings": "Settings",
+      "home": "Home",
+      "surveys": "Surveys",
+      "faq": "FAQ"
+  },
+
+  "sidenav": {
+      "subtitle": "Online surveys",
+      "home": "Home",
+      "profile": "Profile",
+      "surveys": "Surveys",
+      "faq": "FAQ  /  Help",
+      "settings": "Settings",
+
+      "backend": {
+          "title": "Administration",
+          "users": "Users",
+          "pan": "Logins",
+          "groups": "Groups",
+          "surveys": "Surveys",
+          "statistics": "Statistics",
+          "statistic": "statistic"
+      }
+  },
+
+  "logout": {
+      "title": "Logout",
+      "desc": "Do you want to log out?",
+      "btn": "Logout",
+      "btn_stay_here": "Stay here"
+  },
+
+  "faq": {
+      "title": "FAQ",
+      "subtitle": "Frequently asked questions",
+      "text": "If you have any questions left or need some help, contact us via email: <a href='mailto:{email}'>{email}</a>"
+  },
+
+  "management": {
+      "header": "Management"
+  }
 }
