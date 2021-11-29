6c2d7da21cab4700e69b226985d8162e04a42046
jo <jo>
2021-11-29 22:39:19 +0100

    app added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,75 @@
-<template>
-  <router-view />
+<template lang="pug">
+#app
+  loading(ref="loading")
+  transition(name="page", mode="out-in")
+    component(:is="layout", v-if="layout")
 </template>
-<script>
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'App'
-})
+<script>
+import Loading from "@/components/Loading";
+
+// Load layout components dynamically.
+const requireContext = require.context("@/layouts", false, /.*\.vue$/);
+
+const layouts = requireContext
+  .keys()
+  .map((file) => [file.replace(/(^.\/)|(\.vue$)/g, ""), requireContext(file)])
+  .reduce((components, [name, component]) => {
+    components[name] = component.default || component;
+    return components;
+  }, {});
+
+export default {
+  el: "#app",
+
+  components: {
+    Loading,
+  },
+
+  data: () => ({
+    layout: null,
+    defaultLayout: "AppLayout",
+  }),
+
+  metaInfo() {
+    const { appName } = window.config;
+
+    return {
+      title: appName,
+      titleTemplate: `%s · ${appName}`,
+    };
+  },
+
+  created: function () {
+    this.updateViewportHeightOnResize();
+  },
+
+  mounted() {
+    this.$loading = this.$refs.loading;
+  },
+
+  methods: {
+    /**
+     * Set the application layout.
+     *
+     * @param {String} layout
+     */
+    setLayout(layout) {
+      if (!layout || !layouts[layout]) {
+        layout = this.defaultLayout;
+      }
+
+      this.layout = layouts[layout];
+    },
+
+    updateViewportHeightOnResize() {
+      // We listen to the resize event
+      window.addEventListener("resize", () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      });
+    },
+  },
+};
 </script>
